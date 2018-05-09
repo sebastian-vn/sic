99bf8c23ca292edad5207acfe8246ab191f1c9b9
Sebastian Villegas <sebastian.norena@umanizales.edu.co>
2018-05-09 14:10:49 -0500

    sic

---
+++ js/sic_home.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('.tabs').tabs();
+  $('.collapsible').collapsible();
+  $('.tooltipped').tooltip();
+  $('.sidenav').sideNav();
+});
---
+++ js/welcome_sic.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+  for (var i = 0; i < 6; i++) {
+    $("#nivel" + i).fadeIn(6000);
+  }
+
+});
